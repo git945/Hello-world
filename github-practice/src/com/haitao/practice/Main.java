fea15e4e8c5f2d44ab53db472e8d3f266a25a284
git945 <free.luo@qq.com>
2020-07-15 17:46:57 +0800

    Update Main.java

--- github-practice/src/com/haitao/practice/Main.java
+++ github-practice/src/com/haitao/practice/Main.java
@@ -1,14 +1,5 @@
-/**
- * @(#)Main.java, 7月 15, 2020.
- * <p>
- * Copyright 2020 fenbi.com. All rights reserved.
- * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 package com.haitao.practice;
 
-/**
- * @author luohaitao
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello word");
